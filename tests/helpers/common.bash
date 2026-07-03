4df83b7613f23a4551eb0f39ed6745ecf2f689ae
Matthew Tibbits <894144+mtibbits@users.noreply.github.com>
2026-07-03 02:44:50 +0000

    docs: issue-pin isolation model in skel; hermetic fixtures unset both pins (#240)
    Signed-off-by: Matthew Tibbits <894144+mtibbits@users.noreply.github.com>

--- tests/helpers/common.bash
+++ tests/helpers/common.bash
@@ -7,6 +7,9 @@
 #   teardown() { devagent_test_teardown; }
 
 devagent_test_setup() {
+    # #240: a developer shell may pin project/issue (settings.local.json);
+    # fixtures must be hermetic against both.
+    unset DEVAGENT_ACTIVE_PROJECT DEVAGENT_ACTIVE_ISSUE
     DEVAGENT_TMP="$(mktemp -d -t devagent-bats-XXXXXX)"
     export DEVAGENT_TMP
     export HOME="$DEVAGENT_TMP/home"
